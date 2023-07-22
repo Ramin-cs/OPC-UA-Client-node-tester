f8546f33c9b671da371883c5a3a7d19f3e6435d9
Ramin <Ramin@DESKTOP-NKUF0IP>
2023-07-22 11:48:49 +0300

    Add project files.

---
+++ Upc UA Client tag tester/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Upc_UA_Client_tag_tester
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_node = new System.Windows.Forms.RichTextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lw_result = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txt_node
+            // 
+            this.txt_node.Enabled = false;
+            this.txt_node.Location = new System.Drawing.Point(80, 51);
+            this.txt_node.Name = "txt_node";
+            this.txt_node.Size = new System.Drawing.Size(429, 89);
+            this.txt_node.TabIndex = 0;
+            this.txt_node.Text = "ns=2;s=";
+            this.txt_node.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Enabled = false;
+            this.btn_start.Location = new System.Drawing.Point(80, 146);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(429, 45);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Node :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result :";
+            // 
+            // lw_result
+            // 
+            this.lw_result.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lw_result.Alignment = System.Windows.Forms.ListViewAlignment.SnapToGrid;
+            this.lw_result.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lw_result.FullRowSelect = true;
+            this.lw_result.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lw_result.HideSelection = false;
+            this.lw_result.HotTracking = true;
+            this.lw_result.HoverSelection = true;
+            this.lw_result.Location = new System.Drawing.Point(80, 215);
+            this.lw_result.Name = "lw_result";
+            this.lw_result.Size = new System.Drawing.Size(429, 168);
+            this.lw_result.TabIndex = 5;
+            this.lw_result.UseCompatibleStateImageBehavior = false;
+            this.lw_result.View = System.Windows.Forms.View.Details;
+            this.lw_result.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Connection :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "opc.tcp://127.0.0.1:49321";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(441, 25);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(68, 23);
+            this.btn_connect.TabIndex = 8;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 400;
+            // 
+            // main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 402);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lw_result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_node);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "main_form";
+            this.Text = "OPC UA node Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txt_node;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lw_result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Upc UA Client tag tester/Form1.cs
@@ -0,0 +1,194 @@
+﻿using OpcUaHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upc_UA_Client_tag_tester
+{
+    public partial class main_form : Form
+    {
+        public static OpcUaClient opcua_client = new OpcUaClient();
+        public main_form()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!opcua_client.Connected)
+                {
+                    lw_result.Items.Add("Client not connected!");
+                    lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+                    return;
+                }
+                else
+                {
+
+                    try
+                    {
+                        var value = Convert.ToString(opcua_client.ReadNode(txt_node.Text).Value);
+                        lw_result.Items.Add("Node Value : " + value);
+                        lw_result.Items.Add("Trying write access...");
+                        int state = 0;
+                        try
+                        {
+                            //bolean
+                            opcua_client.WriteNode(txt_node.Text, !Convert.ToBoolean(value));
+                            var temp = Convert.ToString(opcua_client.ReadNode(txt_node.Text).Value);
+                            if (temp != value)
+                            {
+                                lw_result.Items.Add("Write access ✓ - Type:Boolean");
+                                lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.LightGreen;
+                                state = 1;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                        try
+                        {
+                            //Int32
+                            opcua_client.WriteNode(txt_node.Text, Convert.ToInt32(value+1));
+                            var temp = Convert.ToString(opcua_client.ReadNode(txt_node.Text).Value);
+                            if (temp != value)
+                            {
+                                lw_result.Items.Add("Write access ✓ - Type:Int32");
+                                lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.LightGreen;
+                     state=2;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                        try
+                        {
+                            //UInt32
+                            opcua_client.WriteNode(txt_node.Text, Convert.ToUInt32(value + 1));
+                            var temp = Convert.ToString(opcua_client.ReadNode(txt_node.Text).Value);
+                            if (temp != value)
+                            {
+                                lw_result.Items.Add("Write access ✓ - Type:UInt32");
+                                lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.LightGreen;
+                                state = 3;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                        var temp2 = Convert.ToString(opcua_client.ReadNode(txt_node.Text).Value);
+                        if (value==temp2)
+                        {
+                            lw_result.Items.Add("Writing Node Value failed!");
+                            lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+                        }
+                        else
+                        {
+                            switch (state)
+                            {
+                                case 1:
+                                  opcua_client.WriteNode(txt_node.Text, Convert.ToBoolean(value));  
+                                    break;
+                                case 2:
+                                    opcua_client.WriteNode(txt_node.Text, Convert.ToInt32(value ));
+                                    break;
+                                case 3:
+                                    opcua_client.WriteNode(txt_node.Text, Convert.ToUInt32(value));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lw_result.Items.Add("Reading Node Value failed!" );
+                        lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+                        lw_result.Items.Add(ex.Message);
+                        lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+                    }
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+               lw_result.Items.Add(ex.Message);
+                lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+            }
+
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(btn_connect.Text=="Connect")
+                {
+                    opcua_client.Disconnect();
+                    opcua_client.ConnectServer(textBox1.Text);
+                    if (!opcua_client.Connected)
+                    {
+                        lw_result.Items.Add("Client connection failed!");
+                        lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+                        btn_start.Enabled =txt_node.Enabled = false;
+                        return;
+                    }
+                    else
+                    {
+                        lw_result.Items.Add("Client connection Successfull!");
+                        btn_connect.Text = "Disconnect";
+                        lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.LightGreen;
+                        btn_start.Enabled = txt_node.Enabled = true;
+
+                    }
+                }
+                else
+                {
+                    opcua_client.Disconnect();
+                    lw_result.Items.Add("Disconnected!");
+                    btn_connect.Text = "Connect";
+                    lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+                    btn_start.Enabled = txt_node.Enabled = false;
+                }
+            
+            }
+            catch (Exception ex)
+            {
+
+                lw_result.Items.Add(ex.Message);
+                lw_result.Items[lw_result.Items.Count - 1].ForeColor = Color.Red;
+            }
+        }
+    }
+}
